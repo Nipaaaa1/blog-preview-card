e6abcf24176b56aff03bb0c1e2c46fc6f35cfd36
Nipaaaa1 <Frostone224@gmail.com>
2024-01-12 08:14:29 +0700

    Finish coding

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,19 @@
 module.exports = {
   content: ["index.html"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brightYellow: "	#f4d04e",
+        grey: "#808080",
+        black: "#121212",
+      },
+      fontFamily: {
+        Figtree: ["Figtree", "sans-serif"],
+      },
+      boxShadow: {
+        card: "8px 8px",
+      },
+    },
   },
   plugins: [],
-}
+};
